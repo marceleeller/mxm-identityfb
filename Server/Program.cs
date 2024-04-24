f7654f48cadc3a5c2e6da6be45b63597d0cc8d23
Marcele Eller <dev.marcele@gmail.com>
2024-04-24 14:01:35 -0300

    feat(identityserver): implements identity server configs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,53 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Server;
+using Server.Data;
+
+var seed = args.Contains("/seed");
+if (seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+}
+
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var service = builder.Services;
+
+if (seed)
+{
+    SeedData.EnsureSeedData(defaultConnectionString);
+}
+
+service.AddDbContext<AspNetIdentityDbContext>(options =>
+{
+    options.UseSqlServer(defaultConnectionString, 
+        b => b.MigrationsAssembly(assembly));
+}); 
+
+service.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
+// identity server configuration
+service.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnectionString,
+                       opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnectionString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseIdentityServer();
 
 app.Run();
